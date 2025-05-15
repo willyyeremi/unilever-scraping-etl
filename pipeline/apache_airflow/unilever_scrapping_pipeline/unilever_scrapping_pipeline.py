8d6ef43882992cc59f44e2347faa1769aea34513
Willy Yeremi <willyyeremi@gmail.com>
2025-05-15 07:53:25 +0700

    build:

--- pipeline/apache_airflow/unilever_scrapping_pipeline/unilever_scrapping_pipeline.py
+++ pipeline/apache_airflow/unilever_scrapping_pipeline/unilever_scrapping_pipeline.py
@@ -1,9 +1,7 @@
-from time import sleep
 import datetime
 import os
 
 from airflow import DAG
-from airflow.operators.python import PythonOperator, PythonVirtualenvOperator
 from airflow.hooks.base import BaseHook
 from airflow.decorators import task_group, task
 from sqlalchemy.engine.url  import URL
